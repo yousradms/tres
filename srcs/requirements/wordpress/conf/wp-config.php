224969140ef47a13617626dcadcbaf57ec6d4d8b
Yousra Doumas <ydoumas@e3r2p3.1337.ma>
2025-11-04 20:40:38 +0100

    a

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,30 @@
+<?php
+
+define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_HOST', getenv('DB_HOST') );
+define( 'WP_HOME', getenv('WP_FULL_URL') );
+define( 'WP_SITEURL', getenv('WP_FULL_URL') );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', true );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
